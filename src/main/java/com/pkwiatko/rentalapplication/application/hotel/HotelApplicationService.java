4e26c1a5db749b4a81c316481b157817e2384e3e
PK <p.kwiatkowski@aox.pl>
2021-05-05 06:05:36 +0200

    Adding new Hotel - move creation of Hotel from HotelApplicationService to HotelFactory.

--- src/main/java/com/pkwiatko/rentalapplication/application/hotel/HotelApplicationService.java
+++ src/main/java/com/pkwiatko/rentalapplication/application/hotel/HotelApplicationService.java
@@ -1,13 +1,11 @@
 package com.pkwiatko.rentalapplication.application.hotel;
 
-import com.pkwiatko.rentalapplication.domain.hotel.Address;
 import com.pkwiatko.rentalapplication.domain.hotel.Hotel;
+import com.pkwiatko.rentalapplication.domain.hotel.HotelFactory;
 
 public class HotelApplicationService {
 
     public void add(String name, String street, String postalCode, String buildingNumber, String city, String country)   {
-        Address address = new Address(street, postalCode, buildingNumber, city, country);
-        Hotel hotel = new Hotel(name, address);
-
+        Hotel hotel = new HotelFactory().create(name, street, postalCode, buildingNumber, city, country);
     }
 }
