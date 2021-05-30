29e8c1baec8a1fc5873799ff1ac5e047a1990d24
PK <p.kwiatkowski@aox.pl>
2021-05-30 11:33:34 +0200

    Adding new HotelRoom - expose REST endpoint to add an hotel room.

---
+++ src/main/java/com/pkwiatko/rentalapplication/infrastructure/web/rest/api/hotelroom/HotelRoomDto.java
@@ -0,0 +1,33 @@
+package com.pkwiatko.rentalapplication.infrastructure.web.rest.api.hotelroom;
+
+import java.util.Map;
+
+public class HotelRoomDto {
+    private final String hotelId;
+    private final int number;
+    private final Map<String , Double> spacesDefinition;
+    private final String description;
+
+    public HotelRoomDto(String hotelId, int number, Map<String, Double> spacesDefinition, String descriptio) {
+        this.hotelId = hotelId;
+        this.number = number;
+        this.spacesDefinition = spacesDefinition;
+        this.description = descriptio;
+    }
+
+    String getHotelId() {
+        return hotelId;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    Map<String, Double> getSpacesDefinition() {
+        return spacesDefinition;
+    }
+
+    String getDescription() {
+        return description;
+    }
+}
